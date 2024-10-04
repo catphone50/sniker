88f722e1ae75ed6e9aa286c51cae226d6d73edcd
catphone50 <catphone50@gmail.com>
2024-10-04 22:46:05 +0200

    add main page

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import News from "../News";
 import styled from "./styles.module.css";
 import ProductCard from "../ProductCard";
+import axios from "axios";
+const URL = "https://66ced76a901aab24841fcad5.mockapi.io//productData";
 
 function Main() {
   const [productsList, setProductsList] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(URL);
+      setProductsList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className={styled.container}>
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,5 +1,23 @@
-function ProductCard() {
-  return <div>Main</div>;
+import styled from "./styles.module.css";
+
+function ProductCard(cartItem) {
+  const handleAddToCart = () => {};
+
+  return (
+    <div className={styled.card}>
+      <img src={cartItem.image} alt={cartItem.name} />
+      <h4 className={styled.name}>{cartItem.name}</h4>
+      <div className={styled.infoContainer}>
+        <div className={styled.priceContainer}>
+          <p className={styled.priceTitle}>PRICE:</p>
+          <p className={styled.price}>{cartItem.price} EUR</p>
+        </div>
+        <button onClick={handleAddToCart} className={styled.button}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
